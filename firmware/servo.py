7b4c0f509fadf9862a369fb9533c932ea91ffebc
LAP1RZE <piotr.laskowski-ext@bshg.com>
2024-12-06 14:35:41 +0100

    Erased waste files and added servo managment file

--- firmware/servo.py
+++ firmware/servo.py
@@ -1,19 +1,33 @@
 import machine
 import time
 
-# Konfiguracja pinu PWM
-servo = machine.PWM(machine.Pin(23), freq=50)  # Pin 2, częstotliwość 50 Hz
+class Servo:
+    def __init__(self, pin, freq=50):
+        """
+        Servo initialization
+        :param pin: Number of pin GPIO connected to the servo
+        :param freq: PWM frequency (default 50Hz for standard servos
+        """
+        self.pin = machine.Pin(pin)
+        self.pwm = machine.PWM(self.pin)
+        self.pwm.freq(freq)
+        self.min_duty = 1024 # Minimum duty cycle for ~1 ms
+        self.max_duty = 8192 # Maximum duty cycle for ~2 ms
+        self.set_angle(90)   # Set servo in neutral position (90 degrees)
 
-# Funkcja ustawiająca kąt serwa
-def set_angle(angle):
-    # Zmiana szerokości impulsu, odpowiednia do kąta
-    # Zakładając, że kąt 0° to 1ms, a kąt 270° to 2ms.
-    # Przekształcamy kąt na szerokość impulsu (zakres 1 ms do 2 ms)
-    pulse_width = int(0 + (angle / 180) * 180)
-    print(pulse_width)
-    servo.duty(pulse_width)
+    def set_angle(self, angle):
+        """
+        Sets angle of servo
+        :param angle: Angle in degrees (0-180)
+        """
+        if not 0 <= angle <= 180:
+            raise ValueError("Angle has to be between 0-180 degrees. ")
 
-# Funkcja do testowego ustawiania kątów w `servo.py`
-def test_servo(angle):
-    set_angle(angle)
-    time.sleep(1)
+        duty = int(self.min_duty + (self.max_duty - self.min_duty) * (angle/180))
+        self.pwm.duty_u16(duty)
+
+    def deinit(self):
+        """
+        Deinitialization of servo by turning off the PWM
+        """
+        self.pwm.deinit()
